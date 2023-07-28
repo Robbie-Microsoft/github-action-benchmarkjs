2310ca55d9a89406fed3af95541d3dc66be80e01
Robert Ginsburg <rgins16@gmail.com>
2023-07-28 13:16:25 -0400

    Introducing fake regression attempt #2

--- examples/benchmarkjs/bench.js
+++ examples/benchmarkjs/bench.js
@@ -3,17 +3,19 @@ const suite = new Benchmark.Suite();
 const { fib } = require('./index');
 
 suite
-    .add('fib(10)', () => {
-        setTimeout(() => {
-            fib(10);
-        }, 2000);
+    .add('fib(10)', async () => {
+        await sleep(2000);
+        fib(10);
     })
-    .add('fib(20)', () => {
-        setTimeout(() => {
-            fib(10);
-        }, 2000);
+    .add('fib(20)', async () => {
+        await sleep(2000);
+        fib(20);
     })
     .on('cycle', (event) => {
         console.log(String(event.target));
     })
     .run();
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
