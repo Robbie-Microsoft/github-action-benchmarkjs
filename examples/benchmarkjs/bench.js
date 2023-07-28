3928a995d26e6da32ac3db4c71a6ab22f8aa8922
Robert Ginsburg <rgins16@gmail.com>
2023-07-28 13:19:20 -0400

    Introducing fake regression attempt #2

--- examples/benchmarkjs/bench.js
+++ examples/benchmarkjs/bench.js
@@ -1,20 +1,41 @@
 const Benchmark = require('benchmark');
 const suite = new Benchmark.Suite();
-const { fib } = require('./index');
+// const { fib } = require('./index');
+
+// suite
+//     .add('fib(10)', async () => {
+//         await sleep(2000);
+//         fib(10);
+//     })
+//     .add('fib(20)', async () => {
+//         await sleep(2000);
+//         fib(20);
+//     })
+//     .on('cycle', (event) => {
+//         console.log(String(event.target));
+//     })
+//     .run();
 
 suite
-    .add('fib(10)', async () => {
-        await sleep(2000);
-        fib(10);
+    .add('RegExp#test', function() {
+        /o/.test('Hello World!');
     })
-    .add('fib(20)', async () => {
-        await sleep(2000);
-        fib(20);
+    .add('String#indexOf', function() {
+        'Hello World!'.indexOf('o') > -1;
     })
-    .on('cycle', (event) => {
+    .add('String#match', function() {
+        !!'Hello World!'.match(/o/);
+    })
+    // add listeners
+    .on('cycle', function(event) {
         console.log(String(event.target));
     })
     .run();
+    // .on('complete', function() {
+    //     console.log('Fastest is ' + this.filter('fastest').map('name'));
+    // })
+    // run async
+    // .run({ 'async': true });
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
